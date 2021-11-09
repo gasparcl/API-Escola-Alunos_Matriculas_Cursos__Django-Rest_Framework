8b45f3dc14752818884ddd2ff2d2cebb5fa18f43
unknown <gasparcl.dev@gmail.com>
2021-11-09 13:32:44 -0300

    Corrigido o import em setup.settings de django-on-heroku para o import correto: django_on_heroku

--- setup/settings.py
+++ setup/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import django-on-heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
